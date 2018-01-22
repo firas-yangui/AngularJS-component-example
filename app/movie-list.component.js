cd51f7e7a4b49d3979c7be2338f2f6da62b062f0
Firas yangui <fyangui@linagora.com>
2018-01-22 18:41:02 +0100

    add fetching data

--- app/movie-list.component.js
+++ app/movie-list.component.js
@@ -4,11 +4,37 @@
   angular.module('psMovies')
     .component('movieList', movieList());
 
+    function movieListController($http) {
+      var self = this;
+
+      self.message = "hello i'm here";
+      self.changeMessage = changeMessage;
+      self.$onInit = $onInit;
+
+      function changeMessage() {
+        console.log('changed');
+        self.message = "new message";
+      }
+
+      function fetchMovies() {
+        return $http.get('movies.json')
+          .then(function(response) {
+            return response.data;
+          });
+      }
+
+      function $onInit() {
+        fetchMovies().then(function(movies) {
+          self.movies = movies;
+        });
+      }
+    }
+
     function movieList() {
       return {
-        templateUrl: "./app/movie-list.component.html",
-        controllerUrl:"./app/movie-list.controller.js",
-        controllerAs: "ctrl"
+        templateUrl: '/app/movie-list.component.html',
+        controllerAs: 'ctrl',
+        controller: ["$http", movieListController]
       };
     }
 })();
--- app/movie-list.controller.js
+++ app/movie-list.controller.js
@@ -4,9 +4,29 @@
   angular.module('psMovies')
     .controller('movieListController', movieListController);
 
-    function movieListController() {
+    function movieListController($http) {
       var self = this;
 
       self.message = "hello i'm here";
+      self.changeMessage = changeMessage;
+      self.$onInit = $onInit;
+
+      function changeMessage() {
+        console.log('changed');
+        self.message = "new message";
+      }
+
+      function fetchMovies() {
+        return $http.get('movies.json')
+          .then(function(response) {
+            return response.data;
+          });
+      }
+
+      function $onInit() {
+        fetchMovies().then(function(movies) {
+          self.movies = movies;
+        });
+      }
     }
 })();
